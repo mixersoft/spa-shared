af8cc4c69ba0fc4366741da70270b2470228b977
michael lin <mlininkl@gmail.com>
2023-02-03 00:07:22 +0100

    initial commit for navbar

--- src/index.js
+++ src/index.js
@@ -17,11 +17,12 @@ const interpolateTemplate = () => {
 
 const htmlLifecycles = singleSpaHtml({
   domElementGetter: () => {
-    const id = "single-spa-application:@example/cookie-consent";
+    const id = "@ml/navbar";
     let container = document.getElementById(id);
     if (!container) {
       container = document.createElement("div");
       container.id = id;
+      container.classList += "flex-top flex-block";
       document.body.prepend(container); // single-spa automatically _appends_, but this content should be _prepended_ for accessibility
     }
     return container;
@@ -40,52 +41,9 @@ This seems complicated so let's break it down:
 export const mount = async (props) => {
   // uses localStorage for convenience but could be anything to check.
   // for example, this could be implemented with cookies if you wanted to send it back and forth to your server
-  const priorConsentOrNull = localStorage.getItem("cookie-consent");
-  const hasPriorConsent = Boolean(priorConsentOrNull); // check for prior consent set by app
-
-  if (hasPriorConsent) {
-    const { date } = JSON.parse(priorConsentOrNull);
-    const priorConsentDate = new Date(date).getTime();
-    // consent may expire so request it again if needed
-    // this also allows me to clear the value for demo purposes
-    const priorConsentExpired = priorConsentDate - Date.now() > ONE_MINUTE;
-    if (priorConsentExpired) {
-      localStorage.removeItem("cookie-consent"); // re-obtain consent if expired
-    } else {
-      return Promise.resolve(null); // don't render anything if user prior consent & it has not expired
-    }
-  }
+ 
   await htmlLifecycles.mount(props); // wait for single-spa to mount the application
 
-  // after app mount, bind behaviors the interactive elements with plain JavaScript
-  const dialog = document.querySelector("#cookie-consent"),
-    noSellCheckbox = dialog?.querySelector("#cookie-consent-no-sell"),
-    acceptBtn = dialog?.querySelector("#cookie-consent-accept");
-
-  if (!dialog || !noSellCheckbox || !acceptBtn) return; // sanity checks
-
-  // bind and handle click event on button
-  acceptBtn.addEventListener("click", function handleConsentAction() {
-    // create consent data object
-    const consent = JSON.stringify({
-      date: new Date().toJSON(),
-      noSell: noSellCheckbox.checked,
-    });
-    localStorage.setItem("cookie-consent", consent);
-    dialog.classList.add(styles.hide); // animate out
-  });
-
-  // remove hide class so that dialog animates in
-  setTimeout(function onInit() {
-    dialog.classList.remove(styles.hide);
-  });
-
-  dialog.addEventListener("transitionend", function handleTransitionEnd() {
-    // listen for when animation ends and set hidden attribute so that they remain in sync
-    dialog.classList.contains(styles.hide) && dialog.setAttribute("hidden", "");
-  });
-
-  // TODO: implement a way to revoke consent (execise left to reader)
 };
 
 export const { bootstrap, unmount } = htmlLifecycles; // export other lifecycles as-is
