34d13f49654b6e0ab508940926ca88217a687899
michael lin <mlininkl@gmail.com>
2023-02-12 18:12:52 +0100

    (fix) parse queryparam values to get booleans

--- src/duo-fr-tagging.js
+++ src/duo-fr-tagging.js
@@ -13,7 +13,7 @@ function getUrlParameter(sParam) {
   for (i = 0; i < sURLVariables.length; i++) {
       sParameterName = sURLVariables[i].split('=');
       if (sParameterName[0] === sParam) {
-          return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+          return sParameterName[1] === undefined ? true : JSON.parse(decodeURIComponent(sParameterName[1]));
       }
   }
   return false;
