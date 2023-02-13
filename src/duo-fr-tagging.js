32c02fd0db92e64713bb77d7c2aa30597aadb496
michael lin <mlininkl@gmail.com>
2023-02-13 14:48:16 +0100

    (fix) clean up dataM editing UX

--- src/duo-fr-tagging.js
+++ src/duo-fr-tagging.js
@@ -40,6 +40,7 @@ export class HalEditor  {
   transcriptEl;
   player;
   isActive = false;
+  halTargetEl;    // span[data-m]
 
   constructor(options={}){
     Object.assign(this.config, DEFAULT_CONFIG, options);
@@ -57,39 +58,147 @@ export class HalEditor  {
 
   logTimings = async (ev)=>{
     const FORCE_TAGGING = !!getUrlParameter('edit')
-    const PLAYBACK_RATE_FOR_TAGGING = 1.9
+    const PLAYBACK_RATE_FOR_TAGGING = 1; // 1.9
     this.player.player.playbackRate = PLAYBACK_RATE_FOR_TAGGING
   
-    const target = ev.target;
-    const dataMms = ev.target.getAttribute('data-m');
+    this.halTargetEl = ev.target;
+    const dataMms = this.halTargetEl.getAttribute('data-m');
+    if (ev.metaKey || ev.shiftKey || ev.altKey){
+      const clip = this.halTargetEl.innerText.split(":")[1].substring(1,20)
+      console.log(`select: data-m=${humanizeMs(dataMms)} , ${clip}`); // true or false
+      return;
+    }
     if (dataMms==="0" || dataMms===null || FORCE_TAGGING) {
       ev.stopImmediatePropagation() // do not cue player
       const playerCurrentTime = await this.player.getTime();
-      const clip = ev.target.innerText.split(":")[1].substring(1,20)
+      const clip = this.halTargetEl.innerText.split(":")[1].substring(1,20)
       if (dataMms!==null){
         const click_offset = -1.00; // reflex delay in sec at speed 1.9x
         const timeMs = (Math.round((playerCurrentTime+click_offset)*1000));
         console.log(`"${clip}...", data-m="${timeMs}", was ${dataMms}`)
-        ev.target.setAttribute('data-m', timeMs)
+        this.halTargetEl.setAttribute('data-m', timeMs)
       } 
     }
   }
+
+  _handleKeydown(ev){
+    // bind this=HalEditor
+    self = this;
+    Promise.resolve(ev)
+    .then( this._checkForReplay.bind(this))
+    .then( (ev)=>{ 
+      if (ev=="done") return Promise.reject("done")
+      return ev;
+    })
+    .then( ev=>{
+      return this._adjustTiming.bind(this)( ev, "data-m")
+    } )
+    .catch( console.log );
+  }
+
+  async _checkForReplay( ev ) {
+    const REPLAY_DURATION_MS=5000;
+    // bind this=HalEditor
+    self = this;
+    if (["Space", "Enter"].includes(ev.code)){
+      ev.preventDefault();
+
+      // return to curTime after replay clip
+      let curTime = await this.player.getTime();
+      const dataMms = this.halTargetEl.getAttribute('data-m');
+      
+      // replay clip at dataM for duration=REPLAY_DURATION_MS
+      this.player.setTime(dataMms/1000)
+      this.player.play()
+      console.log(`replay at data-m=${dataMms}`);
+      clearTimeout(this.config._cancelReplayClip);
+      this.config._cancelReplayClip = setTimeout.bind(this)( ()=>{
+        // then return to curTime after replay clip
+        console.log("return to curTime=", Math.round(curTime*10)/10 );
+        this.player.setTime(curTime);
+        this.player.play();
+      }
+      , REPLAY_DURATION_MS);
+
+      return Promise.resolve("done");
+    }
+    else {
+      // not replay, continue with event processing
+      return ev;
+    }
+  }
+  
+  async _adjustTiming( ev, attr="data-m" ){
+    // bind this=InteractivePlayer
+    const INCREMENT_MS = 300;
+    let check = this // undefined
+    let targetEl = this.halTargetEl;
+    const deltaMs = plusOrMinus(ev) * INCREMENT_MS;
+    if (deltaMs==0) return ev;
+  
+    let curTimeMs = parseInt(targetEl.getAttribute(attr));
+    const timeMs = Math.round(curTimeMs + deltaMs);
+    const clip = targetEl.innerText.split(":")[1].substring(1,20)
+    targetEl.setAttribute(attr, timeMs)
+    console.log(`"${clip}...", ${attr}="${(timeMs)}", was ${(curTimeMs)}+${Math.round(deltaMs)}`)
+    return timeMs;
+  }
+
+  
+  /**
+   * expand span[data-m] to sentences or phrases
+   * @param {*} ev 
+   */
+  expandContent(ev){
+    let target = ev.target;
+    let text = target.textContent;
+    let regex = /\b\w{5,}[.!?]/g;
+    let matches = str.match(regex);
+    console.log(matches);
+  }
+
+
+
+  tagOrExpand(ev){
+    let target = ev.target;
+    console.log("keydown=",ev.code)
+    // let text = target.textContent;
+    // let regex = /[^.!?]+[.!?]/g // /\b\w{5,}[.!?]/g;
+    // let matches = text.match(regex);
+    // console.log(matches);
+  }
+
   /**
    * listen for halEditor click events
    * 
    * @param {*} force 
    * @param {*} cb, (onChange hack) callback function to call in external scope 
-   * @returns 
+   * @returns boolean  true, if edit mode, e.g. this.isActive
    */
   toggleEdit(force=false, cb){
-    this.isActive = force ? force : !this.isActive;
+    const self = this;
+    const action = typeof force=="boolean" ? force : !this.isActive;
     if (typeof cb != "undefined") {
       this.cb = cb;  // "register" callback
     }
+    if (this.isActive==action) {
+      console.log(`toggleEdit() state unchanged`);
+      return;
+    }
+
+    this.isActive = action;
     const method = this.isActive ? 'addEventListener' : 'removeEventListener';
-    this.transcriptEl.querySelectorAll('[data-m]').forEach( el=>el[method]("click", this.logTimings) );
+    this.transcriptEl.querySelectorAll('[data-m]').forEach( el=>{
+      el[method]("click",this.logTimings);
+    });
+    document[method]("keydown"
+      , this._handleKeydown.bind(this)
+      // , this._handleKeydown
+    );
+    
     // debug
     window.check = this
+
     switch (typeof this.cb ) {
       case "function":
         this.cb(this.isActive);
@@ -104,6 +213,43 @@ export class HalEditor  {
     }
   }
 
-} // end class HypertranscriptTagger
+} // end class HalEditor
+
+
+
+
+
+
+function humanizeMs(v){
+  return Math.round(v/100)/10;
+}
+/**
+ * detect document."keydown" event and add or remove time
+ */
+function plusOrMinus(ev){
+  let decode = {
+    "Comma": -1,
+    "Period": 1,
+    "ArrowLeft": -1,
+    "ArrowRight": 1,
+  }
+  return decode[ev.code] || 0;
+}
+
+function handleKeydown(ev, self){
+  // bind this=HalEditor
+  self ||= this;
+  Promise.resolve(ev)
+  .then( this._checkForReplay.bind(this))
+  .then( (ev)=>{ 
+    if (ev=="done") return Promise.reject("done")
+    return ev;
+  })
+  .then( ev=>{
+    return this._adjustTiming.bind(this)( ev, "data-m")
+  } )
+  .catch( console.log );
+}
+
 
 export { HalEditor as default }
